67dfaf4c7942ddb95941244b9442c005a047f0ea
rum2mojito <wead3105@sjtu.edu.cn>
2018-08-04 13:28:00 +0800

    edit ci

--- test/testDownloadTroopData.py
+++ test/testDownloadTroopData.py
@@ -2,14 +2,14 @@ import unittest
 #from gameData.downloadTroopData import downloadTroopData
 import sys
 sys.path.append('home/parallels/Downloads/hacktra/gameData')
-from gameData import downloadTroopData
+from gameData.downloadTroopData import downloadTroopData
 import globalVar
 
 class testDownloadData(unittest.TestCase):
   def test_connection(self):
     for troop in range(len(globalVar.TROOPTYPE)):
       url = globalVar.TROOPURL + globalVar.TROOPTYPE[troop]
-      re = downloadTroopData.downloadTroopData(url, troop)
+      re = downloadTroopData(url, troop)
       troopClass = re.classJudge(troop)
       troopName = re.nameJudge(troop)
       print('Data of ' + troopClass + '(' + troopName + '):')
