e59af94499bc57c895d523727c24c609b1a26d0f
Yuwei <wead3105@sjtu.edu.cn>
2018-08-23 14:22:18 +0800

    Issue19 (#21)
    * #19 using django
    
    * refine project struc
    
    * stop testing
    
    * update readme
    
    * update requirements
    
    * #19 dashboard tmp

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
 
 import datetime
 
 # Create your views here.
 
-def hello_world(request):
-  return render(request, 'hello_world.html', {
-    'current_time': str(datetime.time()),
-  })
+def index(request):
+  return render(request, 'index.html')
